0e8de8cf4c42a983bd15bb08d5199005d73c1528
Colin Yates <colin.yates@qficonsulting.com>
2014-10-30 12:32:27 +0000

    Now works in IE8.

--- project.clj
+++ project.clj
@@ -2,16 +2,17 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2234"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.6.4"]]
+                 [om "0.8.0-alpha1"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ancient "0.5.5"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "om-tut"
               :source-paths ["src"]
               :compiler {
